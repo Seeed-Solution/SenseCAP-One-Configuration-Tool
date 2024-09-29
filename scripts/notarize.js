d7bb6f5436c48647a56f7e45e2b608f12ea0ae29
jerryyip <chdyejiewei@gmail.com>
2024-09-29 20:48:19 +0800

    fix: update notarize.js

--- scripts/notarize.js
+++ scripts/notarize.js
@@ -14,6 +14,7 @@ exports.default = async function notarizing(context) {
     appPath: `${appOutDir}/${appName}.app`,
     appleId: process.env.APPLEID,
     appleIdPassword: process.env.APPLEIDPASS,
-    ascProvider: process.env.APPLEASCPROVIDER
+    ascProvider: process.env.APPLEASCPROVIDER,
+    teamId: process.env.APPLEASCPROVIDER,
   });
 };
